7e8f6e30575043befad6707c37a4cdf4170043ed
nicolas-suarez-97 <nicolas.suarez10@hotmail.com>
2021-12-08 21:14:12 -0500

    Delete unnecessary files

--- json/articles.js
+++
@@ -1,9 +0,0 @@
-import autoScratchRemovers from "./automotive/car-care/auto-scratch-removers";
-import best4kHdmiCables from "./electronics/accessories-supplies/best-4k-hdmi-cables";
-
-const articles = [
-    autoScratchRemovers,
-    best4kHdmiCables
-]
-
-export default articles;
